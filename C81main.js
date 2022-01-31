1d94e2ad440e168564afde58f7e21c30f7b2b153
army2010 <85797689+army2010@users.noreply.github.com>
2022-01-31 04:03:49 -0800

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,28 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+color = "yellow";
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 4;
+ctx.arc(200, 200, 50, 0, 2 * Math.PI);
+ctx.stroke();
+canvas.addEventListener("mousedown", my_mousedown)
+
+function my_mousedown(e) {
+    color = document.getElementById("color").value;
+    mouse_x = e.clientX - canvas.offsetLeft;
+    mouse_y = e.clientY - canvas.offsetTop;
+    circle(mouse_x, mouse_y);
+}
+
+function circle(mouse_x, mouse_y) {
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 4;
+    ctx.arc(mouse_x, mouse_y, 50, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
+function clearArea() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
